aaaf4755a72866d3e5c3dd895b1fdfd9f10ad0e6
bartlett-m <bartlem@stpaulsschool.org.uk>
2024-08-31 14:57:12 +0100

    variable type enums for simplex algorithm

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,5 +1,6 @@
 __all__ = [
     "queueutils",
     "directionenums",
-    "trees"
+    "trees",
+    "variabletypeenums"
 ]
---
+++ utils/variabletypeenums.py
@@ -0,0 +1,9 @@
+"""Enums to represent the type of variable in linear programming equations"""
+
+from enum import Enum
+
+
+class VariableType(Enum):
+    NORMAL = 1
+    SLACK = 2
+    CONSTANT = 3
