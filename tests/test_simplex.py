f03bc387fa0602fab62b880b2e9636b6be2881b2
bartlett-m <bartlem@stpaulsschool.org.uk>
2024-10-29 15:00:17 +0000

    start working on a new test (this one happens to cover an edge case)

--- tests/test_simplex.py
+++ tests/test_simplex.py
@@ -26,3 +26,15 @@ class TestTableau(unittest.TestCase):
                 [0, 0, Fraction(20, 3), Fraction(10, 3), 1, 600]
             ]
         )
+
+    def test_solve_1(self):
+        # when pivoting, this test will try a division by zero
+        t = simplex.Tableau(
+            [
+                simplex.TableauRow([Fraction(x) for x in [1, 1, 1, 1, 0, 0, 0, 10]]),
+                simplex.TableauRow([Fraction(x) for x in [2, -1, 0, 0, 1, 0, 0, 0]]),
+                simplex.TableauRow([Fraction(x) for x in [-1, -3, 1, 0, 0, 1, 0, 6]]),
+                simplex.TableauRow([Fraction(x) for x in [-5, 3, -4, 0, 0, 0, 1, 0]])
+            ]
+        )
+        t.pivot_until_done()
