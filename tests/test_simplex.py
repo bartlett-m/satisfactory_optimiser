1742f16b5a1fe8e904486a6a08b2d3944ceba38c
bartlett-m <bartlem@stpaulsschool.org.uk>
2024-11-19 14:27:20 +0000

    new unit tests for new simplex constructor

--- tests/test_simplex.py
+++ tests/test_simplex.py
@@ -64,3 +64,39 @@ class TestTableau(unittest.TestCase):
                 [0, 0, 0, Fraction(27, 10), Fraction(9, 5), Fraction(13, 10), 1, Fraction(348, 10)]
             ]
         )
+
+    def test_new_constructor_temp(self):
+        # TODO: the print statements within the new constructor have been
+        # removed.  replace them with a proper check here.
+        # also TODO: make the tableau store a header with the variable names
+        # so equality can be checked properly.  possibly also use this header
+        # so the variable names dont have to be sorted in the constructor.
+        t = simplex.Tableau(
+            inequalities=[
+                simplex.Inequality([simplex.Variable("x", 1), simplex.Variable("y", 1), simplex.Variable("z", 1)], 10),
+                simplex.Inequality([simplex.Variable("x", 2), simplex.Variable("y", -1)], 0),
+                simplex.Inequality([simplex.Variable("x", 1), simplex.Variable("y", 3), simplex.Variable("z", -1)], -6),
+                simplex.ObjectiveEquation([simplex.Variable("x", -5), simplex.Variable("y", 3), simplex.Variable("z", -4)], 0, 1)
+            ]
+        )
+
+    def test_temp(self):
+        # actually just me doing further maths discrete classwork
+        # but i found some bugs in my new constructor so it was useful
+        print("test_temp")
+        t = simplex.Tableau(
+            inequalities=[
+                simplex.Inequality([simplex.Variable("x", 5), simplex.Variable("y", 7)], 35),
+                simplex.Inequality([simplex.Variable("x", 4), simplex.Variable("y", 9)], 36),
+                simplex.ObjectiveEquation([simplex.Variable("x", -2), simplex.Variable("y", -3)], 0, 1)
+            ]
+        )
+        t.pivot_until_done()
+        # based on what desmos said doing this program graphically
+        # the output it gave looks correct
+
+        # [Fraction(1, 1), Fraction(0, 1), Fraction(9, 17), Fraction(-7, 17), Fraction(0, 1), Fraction(63, 17)]
+        # [Fraction(0, 1), Fraction(1, 1), Fraction(-4, 17), Fraction(5, 17), Fraction(0, 1), Fraction(40, 17)]
+        # [Fraction(0, 1), Fraction(0, 1), Fraction(6, 17), Fraction(1, 17), Fraction(1, 1), Fraction(246, 17)]
+        for row in t._tableau:
+            print(row)
