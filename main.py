722029a1ed95a2f0c9b4bf717c74158351807092
bartlett-m <bartlem@stpaulsschool.org.uk>
2024-11-29 10:06:23 +0000

    allow passing the path to the docs as an argument and add skeleton for autodetermining the correct path

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import json
-import os
 import logging
+import argparse
+import pathlib
+import sys
 
 from satisfactoryobjects import (
     itemhandler,
@@ -12,10 +14,59 @@ from satisfactoryobjects import (
 
 toplevel_logger = logging.getLogger(__name__)
 
-SATISFACTORY_ROOT = os.path.expanduser(
-    "~/.local/share/Steam/steamapps/common/Satisfactory/"
-)
-SATISFACTORY_DOCS = "CommunityResources/Docs/Docs.json"
+
+def get_default_satisfactory_docs_path() -> pathlib.Path | None:
+    # think this is the same for all satisfactory installations
+    SATISFACTORY_DOCS = pathlib.Path('CommunityResources/Docs/Docs.json')
+    # also if you are reading this and have the time and it is after my nea
+    # deadline and have satisfactory on eric james launcher i would appreciate
+    # it if you checked where it plonked satisfactory, so i can add it as an
+    # option (or you could make a pr)
+    # i only have satisfactory on steam
+    # also if you have the eric james version and use linux could you maybe
+    # test with heroic games launcher and/or lutris-installed eric james
+    # launcher (or some other launcher i havent heard of)
+    match sys.platform:
+        case 'emscripten' | 'wasi':
+            # webassembly
+            return None
+        case 'android':
+            # i think qt for python lets you build android apps but i dont
+            # know how open() would work there (probably not at all with SAF)
+            # this app may work there but isnt really designed for it.
+            # try this one at your own risk.
+            return None
+        case 'ios':
+            # does qt for python even support ios?
+            # would probably be hell to get working even if you do pay the
+            # recurring developer ransom
+            return None
+        case 'linux':
+            # best tested platform.
+            return pathlib.PosixPath(
+                "~/.local/share/Steam/steamapps/common/Satisfactory/"
+            ).expanduser().joinpath(SATISFACTORY_DOCS).resolve()
+        case 'win32' | 'cygwin':
+            # all windows
+            # i think cygwin lets you access windows paths directly
+            # windows is the only platform satisfactory itself officially
+            # supports
+            # TODO: check where the file is on Windows
+            return None
+        case 'darwin':
+            # macos (darwin bsd)
+            # official support not planned but should work
+            # feel free to add the path here and pull request
+            # from whenever my nea deadline has passed
+            # (idk lets say june 2025 will definitely be passed)
+            return None
+        case 'aix':
+            # likely would work decently if the docs file was provided
+            # but i doubt steam supports this
+            return None
+        case _:
+            # default pattern i.e. OS not in python documentation as of writing
+            return None
 
 
 def register_handlers() -> None:
@@ -87,7 +138,7 @@ def register_handlers() -> None:
     logger.debug("Finished registering native class handlers")
 
 
-def load_docs(satisfactory_docs_absolute_path: str) -> None:
+def load_docs(satisfactory_docs_absolute_path: pathlib.Path) -> None:
     """Load Docs.json at the given path and trigger handlers
     WARNING: Inadvisable to call this more than once."""
     logger = toplevel_logger.getChild("load_docs")
@@ -107,21 +158,23 @@ def load_docs(satisfactory_docs_absolute_path: str) -> None:
         nativeclasses.SatisfactoryNativeClassHandler.handle()
 
 
-def main():
+def main(arguments: argparse.Namespace):
     logger = toplevel_logger.getChild("main")
 
+    if arguments.docs_file is None:
+        logger.critical(
+            'Satisfactory docs path could not be autodetermined and was not \
+                provided!'
+        )
+        # see /usr/include/sysexits.h on your linux system for more info
+        sys.exit(64)
     logger.info(
-        f"Satisfactory installation root determined as {SATISFACTORY_ROOT}"
+        f'Satisfactory docs path to use is {arguments.docs_file}'
     )
 
     register_handlers()
 
-    satisfactory_docs_absolute_path = os.path.join(
-        SATISFACTORY_ROOT,
-        SATISFACTORY_DOCS
-    )
-
-    load_docs(satisfactory_docs_absolute_path)
+    load_docs(arguments.docs_file)
 
     print(
         [
@@ -157,6 +210,24 @@ def main():
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog='Satisfactory Optimiser',
+        description='Linear programming solver for Satisfactory item \
+            production'
+    )
+
+    # see
+    # https://dusty.phillips.codes/2018/08/13/python-loading-pathlib-paths-with-argparse/
+    parser.add_argument(
+        '-d', '--docs-file',
+        action='store',
+        type=pathlib.Path,
+        default=get_default_satisfactory_docs_path()
+    )
+
+    passed_arguments = parser.parse_args()
+
     logging.basicConfig(level=logging.DEBUG, filename="last.log", filemode="w")
     toplevel_logger.info("Set log configuration")
-    main()
+
+    main(passed_arguments)
