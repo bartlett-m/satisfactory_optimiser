9ab48da05d7b8f571c9227460f76b960308e8b79
bartlett-m <bartlem@stpaulsschool.org.uk>
2024-12-16 15:33:48 +0000

    complete rewrite of the window code.  use flowlayout for solution, make run optimisation button not be in scroll area, a bunch of stuff i forgot this took so much time debugging, generally improve stuff, generally make stuff worse probably, add a function to make subsection headers for the main form (only a function since a custom widget was not justified for the complexity and also it would have created some border stuff), im not sure this took so long

--- gui/window.py
+++ gui/window.py
@@ -6,10 +6,37 @@ from PySide6.QtWidgets import (
     QScrollArea,
     QVBoxLayout,
     QFormLayout,
-    QPushButton
+    QPushButton,
+    QSizePolicy,
+    QFrame,
+    QComboBox,
+    QHBoxLayout,
+    QDoubleSpinBox,
+    QGroupBox
 )
+from PySide6.QtCore import Qt
+
+from thirdparty.flowlayout import FlowLayout
+
 from .item import Item
 from .targets_widget import TargetsWidget, Target
+
+
+def make_form_subsection_header(
+    subsection_label: str,
+    button_label: str = 'Add'
+) -> tuple[QHBoxLayout, QPushButton]:
+    """Helper function to make the subsection header for a form"""
+    subsection_header_layout = QHBoxLayout()
+    subsection_header_layout.addWidget(QLabel(subsection_label))
+    subsection_header_button = QPushButton(button_label)
+    # make the button fill the space not occupied by the label
+    subsection_header_button.setSizePolicy(
+        QSizePolicy.Policy.MinimumExpanding,
+        QSizePolicy.Policy.Fixed
+    )
+    subsection_header_layout.addWidget(subsection_header_button)
+    return (subsection_header_layout, subsection_header_button)
 
 
 class MainWindow(QMainWindow):
@@ -20,41 +47,102 @@ class MainWindow(QMainWindow):
 
         self.tabs = QTabWidget()
 
-        self.problem_layout = QFormLayout()
-        self.solution_layout = QVBoxLayout()
+        # holds the scroll area and the run optimisation button
+        self.problem_layout = QVBoxLayout()
+        # seemingly needed to make the scroll area play nice with the flow
+        # layout
+        solution_layout_container = QWidget()
+        # is held within a scroll area instead
+        self.solution_layout = FlowLayout(solution_layout_container)
+        # why the inconsistency?  because pyside6 just doesnt want to behave
+        # and i had to rewrite this entire function
 
-        add_target_button = QPushButton("Add")
-        add_target_button.clicked.connect(self.add_target)
-        self.problem_layout.addRow('Targets', add_target_button)
+        # make scrollable regions for the contents of the tabs
+        problem_form_container = QScrollArea()
+        solution_tab_content_widget = QScrollArea()
+
+        # why is the code here so inconsistent and messy?
+        # because i moved stuff around until finding an order that wasnt buggy
+        # what determines whether pyside6 behaves how i want it to or not will
+        # forever be a mystery to me
+        # put the flowlayout wrapper widget in the scrollarea
+        solution_tab_content_widget.setWidget(solution_layout_container)
+
+        # allow the form to dynamically resize as items are added and remove,
+        # rather than squashing new items
+        problem_form_container.setWidgetResizable(True)
+        solution_tab_content_widget.setWidgetResizable(True)
+
+        # widget to contain the problem form
+        problem_form_layout_container = QFrame()
+        # was an actual QFormLayout, but this caused some layout issues that a
+        # QVBoxLayout fixed
+        self.problem_form_layout = QVBoxLayout(problem_form_layout_container)
+
+        # custom widget for production targets
         self.targets_widget = TargetsWidget()
-        self.problem_layout.addRow(self.targets_widget)
 
-        self.problem_layout.addRow('Resource Availability', QPushButton("Add"))
+        # start with one target
+        self.targets_widget.add_target(Target())
 
-        self.problem_layout.addRow(QLabel("Weightings"))
+        # create the header for the production targets section
+        (
+            targets_subsection_header,
+            add_target_button
+        ) = make_form_subsection_header(
+            'Targets'
+        )
+        # setup the callback to add new targets
+        add_target_button.clicked.connect(self.add_target)
+        # add the subsection header
+        self.problem_form_layout.addLayout(targets_subsection_header)
+        # add the subsection content
+        self.problem_form_layout.addWidget(self.targets_widget)
 
-        self.problem_layout.addRow(QPushButton("Run Optimisation"))
+        # create the header for the resource availability section
+        (
+            resource_availability_subsection_header,
+            add_resource_availability_data_button
+        ) = make_form_subsection_header(
+            'Resource Availability'
+        )
+        # TODO: callback
+        # add subsection header
+        self.problem_form_layout.addLayout(
+            resource_availability_subsection_header
+        )
+        # TODO: subsection content
 
-        # why does it work here, and not after solution_widget.setWidget
-        # TODO: figure out above and fix
+        # header for the weightings widget has no add button
+        # since there are few weightings
+        # FIXME: maybe use a groupbox?  would look inconsistent though
+        self.problem_form_layout.addWidget(QLabel("Weightings"))
+
+        # put the layout container widget in the scroll area
+        problem_form_container.setWidget(problem_form_layout_container)
+
+        # add the scroll area to the tab layout
+        self.problem_layout.addWidget(problem_form_container)
+        run_optimisation_button = QPushButton("Run Optimisation")
+        # add the run button outside the scroll area but within the tab layout,
+        # so it is always visible
+        self.problem_layout.addWidget(run_optimisation_button)
+
+        # make a widget to hold the layout of the problem tab
+        problem_tab_content_widget = QWidget()
+        # and add the layout to it
+        problem_tab_content_widget.setLayout(self.problem_layout)
+
+        # add the tabs
+        self.tabs.addTab(problem_tab_content_widget, "Problem")
+        self.tabs.addTab(solution_tab_content_widget, "Solution")
+
+        # testing
         self.solution_layout.addWidget(Item("test1", 1))
         self.solution_layout.addWidget(Item("test2", 2))
         self.solution_layout.addWidget(Item("test3", 3))
 
-        solution_widget = QScrollArea()
-
-        problem_layout_container = QWidget()
-        problem_layout_container.setLayout(self.problem_layout)
-
-        solution_layout_container = QWidget()
-        solution_layout_container.setLayout(self.solution_layout)
-
-        solution_widget.setWidget(solution_layout_container)
-
-        self.tabs.addTab(problem_layout_container, "Problem")
-        self.tabs.addTab(solution_widget, "Solution")
-        # self.tabs.setTabEnabled(1, False)  # like this we can disable a tab
-
+        # set the tab layout as the main widget
         self.setCentralWidget(self.tabs)
 
     def add_target(self):
