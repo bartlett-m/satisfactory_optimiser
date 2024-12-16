18cccf38240fb3e79054b2fc4cba83d92fb42974
bartlett-m <bartlem@stpaulsschool.org.uk>
2024-12-16 16:10:54 +0000

    make it possible to remove targets from the target widget, and fix the weird vertical offset affecting the right hand side of each target by ditching the layout wrapper widget

--- gui/targets_widget.py
+++ gui/targets_widget.py
@@ -5,8 +5,11 @@ from PySide6.QtWidgets import (
     QHBoxLayout,
     QComboBox,
     QDoubleSpinBox,
-    QSizePolicy
+    QSizePolicy,
+    QLayout
 )
+import functools
+
 from .config_constants import SUPPOSEDLY_UNLIMITED_DOUBLE_SPINBOX_MAX_DECIMALS
 
 # CAUTION: this better have been populated by the time Target.__init__()
@@ -31,12 +34,11 @@ class Target():
             QSizePolicy.Policy.MinimumExpanding,
             QSizePolicy.Policy.Minimum
         )
-        self._del_button = QPushButton("-")
-        self.right_side_group = QWidget()
-        right_side_layout = QHBoxLayout()
-        right_side_layout.addWidget(self._value)
-        right_side_layout.addWidget(self._del_button)
-        self.right_side_group.setLayout(right_side_layout)
+        self.del_button = QPushButton("-")
+        self.del_button.setDisabled(True)
+        self.right_side_layout = QHBoxLayout()
+        self.right_side_layout.addWidget(self._value)
+        self.right_side_layout.addWidget(self.del_button)
 
 
 class TargetsWidget(QWidget):
@@ -45,5 +47,34 @@ class TargetsWidget(QWidget):
 
         self.layout_ = QFormLayout(self)
 
+    def set_first_del_button_disabled(self, state: bool):
+        '''Set the disabled flag on the delete button of the first target'''
+        # for first itemAt():
+        # item at 0 is the label, item at 1 is the hboxlayout to its right
+        self.layout_.itemAt(1).itemAt(1).widget().setDisabled(state)
+
     def add_target(self, target: Target):
-        self.layout_.addRow(target.combo_box, target.right_side_group)
+        '''Add a target to the widget'''
+        if self.layout_.rowCount() == 1:
+            # there will be more than one target after adding this one, so
+            # re-enable the button of the first target
+            self.set_first_del_button_disabled(False)
+        # add the target to the layout
+        self.layout_.addRow(target.combo_box, target.right_side_layout)
+        # Set the delete callback
+        target.del_button.clicked.connect(
+            functools.partial(
+                self.remove_target,
+                target.right_side_layout
+            )
+        )
+        if self.layout_.rowCount() > 1:
+            # this is not the only target, so enable its delete button
+            target.del_button.setDisabled(False)
+
+    def remove_target(self, right_side_layout: QLayout):
+        '''Callback for the delete button on targets'''
+        self.layout_.removeRow(right_side_layout)
+        if self.layout_.rowCount() == 1:
+            # only one target now remains, so prevent it from being removed
+            self.set_first_del_button_disabled(True)
