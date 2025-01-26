a231543d9e48f099df7c68100cf06b9e93ae35bd
bartlett-m <bartlem@stpaulsschool.org.uk>
2025-01-26 15:45:36 +0000

    add a constant for the large stretch factor of the detailed view of the solution in the future SolutionTabContent widget

--- gui/config_constants.py
+++ gui/config_constants.py
@@ -14,3 +14,10 @@
 # speed" value in-game
 # (see https://satisfactory.wiki.gg/wiki/Clock_speed#Precision)
 SUPPOSEDLY_UNLIMITED_DOUBLE_SPINBOX_MAX_DECIMALS = 4
+
+# Stretch factor that the detailed view of the solution is given in the
+# QSplitter (well SolutionTabContent), that contains it.
+# Just a very large number so that in practice the quick overview is at
+# minimum size.  Increase if you have a *comically large* display and this
+# doesn't behave as intended.
+LARGE_STRETCH_FACTOR_CONSTANT = 1 << 16
