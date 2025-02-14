66e0df44666572a0dd5b77bbc72906d9d114cf46
bartlett-m <bartlem@stpaulsschool.org.uk>
2025-02-14 09:54:34 +0000

    work on checkboxes to toggle all recipes

--- gui/recipeselector.py
+++ gui/recipeselector.py
@@ -1,4 +1,7 @@
+from functools import partial
+
 from PySide6.QtWidgets import QFormLayout, QCheckBox, QWidget, QHBoxLayout, QGridLayout, QGroupBox
+from PySide6.QtCore import Qt
 
 from satisfactoryobjects.recipes import Recipe
 # CAUTION: these better have been populated already, or things will definitely
@@ -14,6 +17,13 @@ class RecipeSelector(QGridLayout):
     ):
         super(RecipeSelector, self).__init__(*args, **kwargs)
         self.recipe_checkboxes: dict[str, QCheckBox] = dict()
+        self.all_normal_recipe_checkbox = QCheckBox('All normal')
+        self.all_alternate_recipe_checkbox = QCheckBox('All alternate')
+        for checkbox in (
+            self.all_normal_recipe_checkbox,
+            self.all_alternate_recipe_checkbox
+        ):
+            checkbox.setTristate(True)
         normal_group_box = QGroupBox('Normal')
         normal_form = QFormLayout()
         alternate_group_box = QGroupBox('Alternate')
@@ -29,6 +39,16 @@ class RecipeSelector(QGridLayout):
         recipe_names.sort(
             key=lambda id_name_tuple: id_name_tuple[1]
         )
+        # to allow for setting the states of all the normal or all the
+        # alternate recipe checkboxes
+        self.normal_recipe_identifiers: set[str] = set()
+        self.alternate_recipe_identifiers: set[str] = set()
+        # to allow for determining if the all alternate or all normal
+        # checkboxes need to be updated
+        self.normal_recipes_available = 0
+        self.alternate_recipes_available = 0
+        self.normal_recipes_active = 0
+        self.alternate_recipes_active = 0
         for id_recipe_tuple in map(
             lambda id_name_tuple: (
                 id_name_tuple[0],
@@ -51,8 +71,23 @@ class RecipeSelector(QGridLayout):
             checkbox.setChecked(not id_recipe_tuple[1].is_alternate)
             # will modify to be backed by something in the config
 
+            checkbox.checkStateChanged.connect(
+                partial(
+                    self.generic_recipe_checkbox_callback,
+                    id_recipe_tuple[0],
+                    id_recipe_tuple[1].is_alternate
+                )
+            )
+
             # self.addRow(id_recipe_tuple[1].user_facing_name, checkbox)
             self.recipe_checkboxes[id_recipe_tuple[0]] = checkbox
+
+            if id_recipe_tuple[1].is_alternate:
+                self.alternate_recipe_identifiers.add(id_recipe_tuple[0])
+                self.alternate_recipes_available += 1
+            else:
+                self.normal_recipe_identifiers.add(id_recipe_tuple[0])
+                self.normal_recipes_available += 1
 
         for group_box, form in (
             (normal_group_box, normal_form),
@@ -60,5 +95,15 @@ class RecipeSelector(QGridLayout):
         ):
             group_box.setLayout(form)
 
+        self.addWidget(self.all_normal_recipe_checkbox, 0, 0)
+        self.addWidget(self.all_alternate_recipe_checkbox, 0, 1)
         self.addWidget(normal_group_box, 1, 0)
         self.addWidget(alternate_group_box, 1, 1)
+
+    def generic_recipe_checkbox_callback(
+        self,
+        recipe_checkbox_id: str,
+        is_alternate: bool,
+        check_state: Qt.CheckState
+    ):
+        print(recipe_checkbox_id)
